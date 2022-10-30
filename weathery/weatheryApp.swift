b8ec30d6662feac6df7df2176834dca1cb98bdac
BEK ROZ <bekrozikoff@gmail.com>
2022-10-30 13:23:35 +0500

    Initial Commit

---
+++ weathery/weatheryApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatheryApp.swift
+//  weathery
+//
+//  Created by BekRoz on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct weatheryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
