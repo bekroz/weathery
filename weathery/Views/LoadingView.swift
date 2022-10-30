8bd964364b4f1facc1973623ef561439573d1e62
BEK ROZ <bekrozikoff@gmail.com>
2022-10-30 18:18:18 +0500

    initial: LocationManager, WeatherView

---
+++ weathery/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  weathery
+//
+//  Created by BekRoz on 30/10/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
